1cc5f490f3ceada35f248a366803fba66ffbb83a
andre-carbajal <carbajalvargasandre@gmail.com>
2024-04-14 21:32:18 -0500

    Update user agent and URL in main.js

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
 const { app, BrowserWindow, shell, session } = require("electron");
 const { registerMenuHandling } = require("./menuBarHandling");
 
+const USER_AGENT = "Chrome";
+const URL = "https://xtiles.app";
+
 app.on("ready", () => {
 
   session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
-    details.requestHeaders["User-Agent"] = "Chrome";
+    details.requestHeaders["User-Agent"] = USER_AGENT;
     callback({ cancel: false, requestHeaders: details.requestHeaders });
   });
 
-  const win = new BrowserWindow({
+  const windowOptions = {
     autoHideMenuBar: true,
     webPreferences: {
       spellcheck: false,
     },
-  });
+  };
+
+  const win = new BrowserWindow(windowOptions);
   win.maximize();
 
-  session.defaultSession.setUserAgent("Chrome");
+  session.defaultSession.setUserAgent(USER_AGENT);
 
-  win.loadURL("https://xtiles.app/user/login", { userAgent: "Chrome" });
+  win.loadURL(`${URL}/user/login`, { userAgent: USER_AGENT });
 
   registerMenuHandling(win);
 
   win.webContents.setWindowOpenHandler(({ url }) => {
-    if (!url.startsWith("https://xtiles.app")) {
+    if (!url.startsWith(URL)) {
       shell.openExternal(url);
       return { action: "deny" };
     }
