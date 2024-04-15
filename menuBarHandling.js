c7fde17d90c1ee960a64f866be8e946dc9069117
andre-carbajal <carbajalvargasandre@gmail.com>
2024-04-14 21:33:49 -0500

    Add menu bar hide functionality and refactor menu handling in menuBarHandling.js

--- menuBarHandling.js
+++ menuBarHandling.js
@@ -1,6 +1,7 @@
 const { globalShortcut, Menu, MenuItem } = require("electron");
 
 let isMenuOnAltBackslash = false;
+const MENU_SHORTCUT_KEY = "Alt+\\";
 
 function toggleMenuBar(win) {
   const isMenuBarVisible = win.autoHideMenuBar;
@@ -8,13 +9,18 @@ function toggleMenuBar(win) {
   win.setMenuBarVisibility(isMenuBarVisible);
 }
 
+function hideMenuBar(win) {
+  win.setAutoHideMenuBar(true);
+  win.setMenuBarVisibility(false);
+}
+
 function registerMenuHandling(win) {
-  const menuShortcut = globalShortcut.register("Alt+\\", () => {
+  const menuShortcut = globalShortcut.register(MENU_SHORTCUT_KEY, () => {
     toggleMenuBar(win);
   });
 
   if (!menuShortcut) {
-    console.error("Failed to register global shortcut!");
+    throw new Error("Failed to register global shortcut!");
   }
 
   win.webContents.on("before-input-event", (event, input) => {
@@ -27,13 +33,12 @@ function registerMenuHandling(win) {
   const windowMenu = menu.items?.find((el) => el.role === "windowmenu");
   if (windowMenu) {
     windowMenu.submenu.insert(0, new MenuItem({
-      label: "Open menu bar on ALT+\\",
+      label: `Open menu bar on ${MENU_SHORTCUT_KEY}`,
       type: 'checkbox',
       checked: isMenuOnAltBackslash,
       click: () => {
         isMenuOnAltBackslash = !isMenuOnAltBackslash;
-        win.setAutoHideMenuBar(true);
-        win.setMenuBarVisibility(false);
+        hideMenuBar(win);
       }
     }));
   }
