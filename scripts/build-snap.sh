00deced18b1d12d1210ad261c73e978a10adae06
andre-carbajal <carbajalvargasandre@gmail.com>
2026-08-06 00:31:07 -0500

    fix: pack snaps from prebuilt prime directories

--- scripts/build-snap.sh
+++ scripts/build-snap.sh
@@ -10,6 +10,7 @@ esac
 
 VERSION="$(node -p "require('./package.json').version")"
 PROJECT_DIR="release/snap-${ARCH}"
+PRIME_DIR="${PROJECT_DIR}/prime"
 if [[ "$ARCH" == "x64" ]]; then
   UNPACKED_DIR="linux-unpacked"
 else
@@ -18,22 +19,21 @@ fi
 OUTPUT="../xtiles-snap_${VERSION}_${ARCH}.snap"
 
 rm -rf "$PROJECT_DIR"
-mkdir -p "$PROJECT_DIR/snap/gui" "$PROJECT_DIR/app"
-cp snap/snapcraft.yaml "$PROJECT_DIR/snapcraft.yaml"
-cp build/icons/icon.png "$PROJECT_DIR/snap/gui/icon.png"
+mkdir -p "$PRIME_DIR/meta/gui"
 
 mkdir -p "release/${UNPACKED_DIR}/usr/share/applications"
 mkdir -p "release/${UNPACKED_DIR}/usr/share/pixmaps"
 cp xtiles-snap.desktop "release/${UNPACKED_DIR}/usr/share/applications/xtiles-snap.desktop"
 cp build/icons/icon.png "release/${UNPACKED_DIR}/usr/share/pixmaps/xtiles-snap.png"
-cp -a "release/${UNPACKED_DIR}/." "$PROJECT_DIR/app/"
+cp -a "release/${UNPACKED_DIR}/." "$PRIME_DIR/"
+cp build/icons/icon.png "$PRIME_DIR/meta/gui/icon.png"
+cp snap/prime-snap.yaml "$PRIME_DIR/meta/snap.yaml"
 
 sed -i \
   -e "s|__VERSION__|${VERSION}|g" \
   -e "s|__ARCH__|${SNAP_ARCH}|g" \
-  "$PROJECT_DIR/snapcraft.yaml"
+  "$PRIME_DIR/meta/snap.yaml"
 
 pushd "$PROJECT_DIR" >/dev/null
-snapcraft prime --destructive-mode
 snapcraft pack --output "$OUTPUT" prime
 popd >/dev/null
